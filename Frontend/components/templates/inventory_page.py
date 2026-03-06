c71f8edd83a315d9620d7178ae2ab9c52d734985
Samuel Esquivel <samuelcuidaesquivel@gmail.com>
2026-03-06 18:37:52 -0500

    Feat: Implement inventory page template with title and inventory table import

---
+++ Frontend/components/templates/inventory_page.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from components.organism.inventory_table import inventory_table
+
+def inventory_page():
+    
+    st.title("Inventario de la ferreteria")
+    inventory_page()
