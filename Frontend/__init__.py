b457d221991d22c63dd0d5fd15a1fbc17cedd008
Samuel Esquivel <samuelcuidaesquivel@gmail.com>
2026-03-06 21:12:17 -0500

    Feat: Add initial component exports for frontend module

---
+++ Frontend/__init__.py
@@ -0,0 +1,6 @@
+from Frontend.components.atoms import button
+from Frontend.components.molecules import product_card
+from Frontend.components.organism import inventory_table
+from Frontend.components.templates import inventory_page
+
+__all__ = ["button", "product_card", "inventory_table", "inventory_page"]
