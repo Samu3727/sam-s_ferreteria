8b46070b5ebf83a7848935e021e2dc8a6d1b7d7a
Samuel Esquivel <samuelcuidaesquivel@gmail.com>
2026-03-07 14:09:00 -0500

    Feat: Implement sidebar navigation and add inventory, sales, and reports pages Chore: Add VSCode settings for Python file associations Add: Compile Python bytecode for button, product card, inventory table, and inventory page components

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from Frontend.components.templates.inventory_page import inventory_page
+from components.templates.inventory_page import inventory_page
 from Backend.services.producto_service import listar_productos
 
 st.set_page_config(page_title="Sam's Ferreteria", layout="wide")
@@ -9,3 +9,16 @@ st.sidebar.title("Menú")
 menu = st.sidebar.radio("Navegación", ["Inventario", "Ventas", "Reportes"])
 
 
+if menu == "Inventario":
+    
+    inventory_page()
+    
+elif menu == "Ventas":
+    
+    st.title("Registrar Venta")
+    st.write("Página de Ventas en construcción... 🛠️")
+    
+elif menu == "Reportes":
+    
+    st.title("Reportes")
+    st.write("Página de Reportes en construcción... 🛠️")
