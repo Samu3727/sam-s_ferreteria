802c30198ec6a7377806a706461ea559fb4c091c
Samuel Esquivel <samuelcuidaesquivel@gmail.com>
2026-03-08 17:36:26 -0500

    Fix: Correct cursor execution method in listar_usuarios function

--- Backend/services/usuarios_service.py
+++ Backend/services/usuarios_service.py
@@ -1,6 +1,11 @@
 from Backend.database.database import get_connection
+from Backend.models.usuarios import Usuario
 
 def listar_usuarios():
     
     conn = get_connection()
-    cur
+    cursor = conn.cursor()
+    cursor.execut("SELECT nombre, correo, contrasena FROM usuarios WHERE estado = 1")
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
