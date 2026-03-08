29e56e777891d5477b2693d9c636a752da09fa76
Samuel Esquivel <samuelcuidaesquivel@gmail.com>
2026-03-08 17:51:15 -0500

    Feat: Add user creation and listing in test_backend function

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,8 @@
 from Backend.database.database import get_connection
 from Backend.services.producto_service import listar_productos, agregar_producto
+from Backend.services.usuarios_service import listar_usuarios, agregar_usuario
 from Backend.models.producto import Producto
+from Backend.models.usuarios import Usuario
 
 def test_backend():
     
@@ -15,8 +17,18 @@ def test_backend():
     productos = listar_productos()
     print("Stock Actual en la ferreteria: ")
     
+    usuario = Usuario("Samuel Esquivel", "samuelcuidaesquivel@gmail.com", "SAOanime37")
+    agregar_usuario(usuario)
+    print("Usuario agregado exitosamente.")
+    
+    usuarios = listar_usuarios()
+    print("Usuario registrados en el sistema.")
+    
     for p in productos:
         print(p)
+        
+    for u in usuarios:
+        print(u)
         
 
 if __name__ == "__main__":
