ad8bba0be5a5cd1dd401853cefcc0566b350e38d
Samuel Esquivel <samuelcuidaesquivel@gmail.com>
2026-03-08 19:08:53 -0500

    Feat: Initialize usuario_en_edicion in session state for users_table component

--- Frontend/components/organism/users_table.py
+++ Frontend/components/organism/users_table.py
@@ -1,3 +1,12 @@
 import streamlit as st
 from Backend.services import usuarios_service
 from Backend.models.usuarios import Usuario
+
+def users_table():
+    
+    if "usuario_en_edicion" not in st.session_state:
+        
+        st.session_state.usuario_en_edicion = None
+        
+        
+    
