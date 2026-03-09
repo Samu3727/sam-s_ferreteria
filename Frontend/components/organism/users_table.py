2fb61c15f693d16d79a79213733411fe8a50a6f1
Samuel Esquivel <samuelcuidaesquivel@gmail.com>
2026-03-08 19:13:22 -0500

    Feat: Enhance users_table component to display user information with proper columns

--- Frontend/components/organism/users_table.py
+++ Frontend/components/organism/users_table.py
@@ -9,4 +9,16 @@ def users_table():
         st.session_state.usuario_en_edicion = None
         
         
+    st.subheader("👥 Nuestro Usuarios.")
     
+    usuarios = usuarios_service.listar_usuarios()
+    
+    if usuarios:
+        
+        col1, col2, col3, col4, col5 = st.columns([3, 2, 2, 2, 2])
+        
+        with col1: st.markdown("**Nombre**")
+        with col2: st.markdown("**Correo**")
+        with col3: st.markdown("**Contraseña**")
+        with col4: st.markdown("**Eliminar**")
+        with col5: st.markdown("**Actualizar**")
