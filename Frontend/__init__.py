3b90a4706ae74faa0f3cd1e85df8c41b27cd35d2
Samuel Esquivel <samuelcuidaesquivel@gmail.com>
2026-03-11 12:16:00 -0500

    Feat: Update __init__.py to include proveedores_table and proveedores_page in exports

--- Frontend/__init__.py
+++ Frontend/__init__.py
@@ -1,6 +1,6 @@
 from Frontend.components.atoms import button
 from Frontend.components.molecules import product_card
-from Frontend.components.organism import inventory_table, users_table
-from Frontend.components.templates import inventory_page, users_page
+from Frontend.components.organism import inventory_table, users_table, proveedores_table
+from Frontend.components.templates import inventory_page, users_page, proveedores_page
 
-__all__ = ["button", "product_card", "inventory_table", "users_table", "inventory_page", "users_page"]
+__all__ = ["button", "product_card", "inventory_table", "proveedores_table", "proveedores_page", "users_table", "inventory_page", "users_page"]
