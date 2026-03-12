9323c58a8988637af931eb0edeb5580aae2e205d
Samuel Esquivel <samuelcuidaesquivel@gmail.com>
2026-03-11 19:39:25 -0500

    Feat: Add imagen parameter to Usuario constructor for enhanced user data management

--- Backend/models/usuarios.py
+++ Backend/models/usuarios.py
@@ -1,7 +1,8 @@
 class Usuario:
     
-    def __init__(self, nombre, correo, contrasena):
+    def __init__(self, nombre, correo, contrasena, imagen):
         
         self.nombre = nombre
         self.correo = correo
         self.contrasena = contrasena
+        self.imagen = imagen
