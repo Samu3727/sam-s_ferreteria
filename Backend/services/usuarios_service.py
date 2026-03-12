1eb9abb6b6d3d74d9ca5f517803b839c6cad9dbd
Samuel Esquivel <samuelcuidaesquivel@gmail.com>
2026-03-11 19:42:31 -0500

    Feat: Enhance user management by adding imagen parameter to relevant functions

--- Backend/services/usuarios_service.py
+++ Backend/services/usuarios_service.py
@@ -5,7 +5,7 @@ def listar_usuarios():
     
     conn = get_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT nombre, correo, contrasena FROM usuarios WHERE estado = 1")
+    cursor.execute("SELECT nombre, correo, contrasena, imagen FROM usuarios WHERE estado = 1")
     rows = cursor.fetchall()
     conn.close()
     return rows
@@ -15,18 +15,18 @@ def agregar_usuario(usuario: Usuario):
     
     conn = get_connection()
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO usuarios (nombre, correo, contrasena) VALUES (%s, %s, %s)",
-                    (usuario.nombre, usuario.correo, usuario.contrasena))
+    cursor.execute("INSERT INTO usuarios (nombre, correo, contrasena, imagen) VALUES (%s, %s, %s)",
+                    (usuario.nombre, usuario.correo, usuario.contrasena, usuario.imagen))
     
     conn.commit()
     conn.close()
     
     
-def eliminar_usuario(nombre: str, correo: str, contrasena: str):
+def eliminar_usuario(nombre: str, correo: str, contrasena: str, imagen: str):
     
     conn = get_connection()
     cursor = conn.cursor()
-    cursor.execute("UPDATE usuarios SET estado = 0 WHERE nombre = %s AND correo = %s AND contrasena = %s AND estado = 1 LIMIT 1", (nombre, correo, contrasena))
+    cursor.execute("UPDATE usuarios SET estado = 0 WHERE nombre = %s AND correo = %s AND contrasena = %s AND imagen = %s AND estado = 1 LIMIT 1", (nombre, correo, contrasena, imagen))
     
     conn.commit()
     filas_afectadas = cursor.rowcount
@@ -34,11 +34,11 @@ def eliminar_usuario(nombre: str, correo: str, contrasena: str):
     return filas_afectadas
 
 
-def actualizar_usuario(nombre: str, nuevo_nombre: str, nuevo_correo: str, nuevo_contrasena: str):
+def actualizar_usuario(nombre: str, nuevo_nombre: str, nuevo_correo: str, nuevo_contrasena: str, nuevo_imagen: str):
     
     conn = get_connection()
     cursor = conn.cursor()
-    cursor.execute("UPDATE usuarios SET nombre = %s, correo = %s, contrasena = %s WHERE nombre = %s AND estado = 1 LIMIT 1", (nuevo_nombre, nuevo_correo, nuevo_contrasena, nombre))
+    cursor.execute("UPDATE usuarios SET nombre = %s, correo = %s, contrasena = %s, imagen = %s WHERE nombre = %s AND estado = 1 LIMIT 1", (nuevo_nombre, nuevo_correo, nuevo_contrasena, nuevo_imagen, nombre))
     
     conn.commit()
     filas_afectadas = cursor.rowcount
