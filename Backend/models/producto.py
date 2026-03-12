42d57d91355c2e50b2fde2c70fb88a398971c474
Samuel Esquivel <samuelcuidaesquivel@gmail.com>
2026-03-11 19:00:21 -0500

    Feat: Update Producto class constructor to include imagen parameter

--- Backend/models/producto.py
+++ Backend/models/producto.py
@@ -1,7 +1,8 @@
 class Producto:
     
-    def __init__(self, nombre, precio, stock):
+    def __init__(self, nombre, precio, stock, imagen):
         
         self.nombre = nombre
         self.precio = precio
         self.stock = stock
+        self.imagen = imagen
